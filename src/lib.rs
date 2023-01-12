cb53ae6f92e08d3cf9a17e82ea09f7a409ec62b6
Grant Surlyn <grant@sunflowerseastar.com>
2023-01-11 22:12:17 -0500

    add click to zoom; more parameters; separate scale fn

--- src/lib.rs
+++ src/lib.rs
@@ -20,61 +20,40 @@ pub struct Universe {
     height: u32,
     iterations: u16,
     cells: Vec<u16>,
-    bx_min: f64,
-    by_min: f64,
-    bx_max: f64,
-    by_max: f64,
+    offsetx: i32,
+    offsety: i32,
+    panx: i32,
+    pany: i32,
+    zoom: f64,
 }
 
 #[wasm_bindgen]
 impl Universe {
-    pub fn tick(&mut self) {
-        let mut next = self.cells.clone();
-
-        for row in 0..self.height {
-            for col in 0..self.width {
-                let idx = self.get_index(row, col);
-                let cell = self.cells[idx];
-
-                let next_cell = self.mandelbrot_value(row, col);
-
-                next[idx] = next_cell;
-            }
-        }
-
-        self.cells = next;
+    fn get_index(&self, x: u32, y: u32) -> usize {
+        (y * self.width + x) as usize
     }
 
-    fn get_index(&self, row: u32, column: u32) -> usize {
-        (row * self.width + column) as usize
+    fn scale_xy(&self, pixel_x: u32, pixel_y: u32, i: usize) -> (f64, f64) {
+        // scaled x coordinate, x0
+        let pixel_x_i32 = pixel_x as i32;
+        let x0_0 = pixel_x_i32 + self.offsetx + self.panx + 1;
+        let x0_1 = x0_0 as f64;
+        let x0 = x0_1 / self.zoom;
+        // scaled y coordinate, y0
+        let pixel_y_i32 = pixel_y as i32;
+        let y0_0 = pixel_y_i32 + self.offsety + self.pany + 1;
+        let y0_1 = y0_0 as f64;
+        let y0 = y0_1 / self.zoom;
+
+        (x0, y0)
     }
 
-    fn scale_to_bounds(&self, y: u32, x: u32) -> (f64, f64) {
-        let bx_span = self.bx_max - self.bx_min;
-        let by_span = self.by_max - self.by_min;
+    fn mandelbrot_value(&self, x0: f64, y0: f64, i: usize) -> u16 {
+        // if (i == 0 || i == (self.width as usize * self.height as usize) - 1) {
+        //     log!("{pixel_x} {pixel_y} {x0} {y0} {i} {x0} {y0}");
+        // }
 
-        let bx_min_abs = self.bx_min.abs();
-        let xf = x as f64;
-        let cwf = self.width as f64;
-        let x01 = xf / cwf;
-        let xx1 = bx_span * x01;
-        let xx = xx1 - bx_min_abs;
-
-        let by_min_abs = self.by_min.abs();
-        let yf = y as f64;
-        let chf = self.height as f64;
-        let y01 = yf / chf;
-        let yy1 = by_span * y01;
-        let yy = yy1 - by_min_abs;
-
-        (xx, yy)
-    }
-
-    fn mandelbrot_value(&self, row: u32, column: u32) -> u16 {
-        let scaled_row_column = self.scale_to_bounds(row, column);
-        let x0 = scaled_row_column.0;
-        let y0 = scaled_row_column.1;
-
+        // determine the escape time of the coordinate
         let mut iteration = 0;
         let mut x = 0.0;
         let mut y = 0.0;
@@ -88,37 +67,49 @@ impl Universe {
         iteration
     }
 
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let idx = self.get_index(x, y);
+                let cell = self.cells[idx];
+                let scaled_xy = self.scale_xy(x, y, idx);
+                let next_cell = self.mandelbrot_value(scaled_xy.0, scaled_xy.1, idx);
+                // log!("{next_cell}");
+                next[idx] = next_cell;
+            }
+        }
+        // log!("len {}", next.len());
+        // log!("{next.0}")
+        self.cells = next;
+    }
+
     pub fn new(
         width: u32,
         height: u32,
         iterations: u16,
-        bx_min: f64,
-        by_min: f64,
-        bx_max: f64,
-        by_max: f64,
+        zoom: f64,
+        offsetx: i32,
+        offsety: i32,
+        panx: i32,
+        pany: i32,
     ) -> Universe {
         let cells = (0..width * height)
             .map(|i| if i % 2 == 0 || i % 7 == 0 { 1 } else { 0 })
             .collect();
+        let bounds = vec![0.0f64, 0.0, 0.0, 0.0];
 
         Universe {
             width,
             height,
             iterations,
+            zoom,
             cells,
-            bx_min,
-            by_min,
-            bx_max,
-            by_max,
+            offsetx,
+            offsety,
+            panx,
+            pany,
         }
-    }
-
-    pub fn width(&self) -> u32 {
-        self.width
-    }
-
-    pub fn height(&self) -> u32 {
-        self.height
     }
 
     pub fn cells(&self) -> *const u16 {
--- www/src/mandelbrot-set.js
+++ www/src/mandelbrot-set.js
@@ -2,13 +2,15 @@ import { Universe } from "wasm-mandelbrot-set";
 import { memory } from "wasm-mandelbrot-set/wasm_mandelbrot_set_bg";
 
 import { interpolateInferno } from "d3-scale-chromatic";
-import { scaleLog, scaleLinear, scaleSqrt, scalePow } from "d3-scale";
+import { scaleSqrt, scalePow } from "d3-scale";
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 const width = canvas.clientWidth;
 const height = canvas.clientHeight;
+// const width = 10;
+// const height = 10;
 canvas.width = width;
 canvas.height = height;
 
@@ -20,15 +22,26 @@ const boundingBox2 = [
   [-0.87, 0.0],
   [-0.67, 0.205],
 ];
-const [[bxMin, byMin], [bxMax, byMax]] = boundingBox1;
-// const [[bxMin, byMin], [bxMax, byMax]] = boundingBox2;
-const areColorsReversed = false;
-const iterations = 500;
+// const [[bxMin, byMin], [bxMax, byMax]] = boundingBox1;
+const [[bxMin, byMin], [bxMax, byMax]] = boundingBox2;
+// const areColorsReversed = false;
+const iterations = 255;
+
+let zoom = 260;
+let offsetx = -width / 2;
+let offsety = -height / 2;
+let panx = -140;
+let pany = 0;
 
 const universe = Universe.new(
   width,
   height,
   iterations,
+  zoom,
+  offsetx,
+  offsety,
+  panx,
+  pany,
   bxMin,
   byMin,
   bxMax,
@@ -41,52 +54,24 @@ const getIndex = (row, column) => {
   return row * width + column;
 };
 
-const color = scaleLinear().domain([0, 500]).range([0, 1]);
-console.log("color(0)", color(0));
-console.log("color(200)", color(200));
-console.log("color(400)", color(400));
-
 const colorSqrt = scaleSqrt().domain([0, iterations]).range([0, 1]);
-console.log("colorSqrt(0)", colorSqrt(0));
-console.log("colorSqrt(200)", colorSqrt(200));
-console.log("colorSqrt(400)", colorSqrt(400));
-
-const colorLog = scaleLog().base(2).domain([0, iterations]);
-console.log("colorLog(0)", colorLog(0));
-console.log("colorLog(200)", colorLog(200));
-console.log("colorLog(400)", colorLog(400));
-
-const colorPow = scalePow().exponent(8).domain([0, iterations]).range([0, 1]);
-console.log("colorPow(0)", colorPow(0));
-console.log("colorPow(200)", colorPow(200));
-console.log("colorPow(400)", colorPow(400));
+// const colorPow = scalePow().exponent(8).domain([0, iterations]).range([0, 1]);
 
 const drawCells = () => {
   const cellsPtr = universe.cells();
+  // console.log("before cells", memory.buffer);
   const cells = new Uint16Array(memory.buffer, cellsPtr, width * height);
+  // console.log("cells", cells);
 
   ctx.beginPath();
 
   for (let row = 0; row < height; row++) {
     for (let col = 0; col < width; col++) {
       const idx = getIndex(row, col);
-      // console.log(cells[idx]);
-
-      // ctx.fillStyle = `rgba(0, 0, 0, ${cells[idx] / 255})`;
-      // ctx.fillStyle = interpolateCubehelixDefault(cells[idx] / iterations)
-
-      // ctx.fillStyle = interpolateBlues(colorLog(cells[idx]));
-      // ctx.fillStyle = interpolateCubehelixDefault(colorLog(cells[idx]));
-      // ctx.fillStyle = interpolateCubehelixDefault(colorSqrt(cells[idx]));
-      // ctx.fillStyle = interpolateRainbow(colorSqrt(cells[idx]));
-      // ctx.fillStyle = interpolateRainbow(colorSqrt(iterations - cells[idx]));
-      // ctx.fillStyle = interpolateCubehelixDefault(colorPow(cells[idx]));
-      // ctx.fillStyle = interpolateRainbow(colorPow(iterations - cells[idx]));
 
       // ctx.fillStyle = interpolateInferno(
       //   colorPow(areColorsReversed ? iterations - cells[idx] : cells[idx])
       // );
-
       ctx.fillStyle = interpolateInferno(colorSqrt(cells[idx]));
 
       ctx.fillRect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE);
@@ -96,13 +81,79 @@ const drawCells = () => {
   ctx.stroke();
 };
 
-const renderLoop = () => {
+const zoomFractal = (x, y, factor, zoomin) => {
+  if (zoomin) {
+    // Zoom in
+    zoom *= factor;
+    panx = factor * (x + offsetx + panx);
+    pany = factor * (y + offsety + pany);
+  } else {
+    // Zoom out
+    zoom /= factor;
+    panx = (x + offsetx + panx) / factor;
+    pany = (y + offsety + pany) / factor;
+  }
+};
+const onMouseDown = (e) => {
+  const pos = getMousePos(canvas, e);
+
+  // Zoom out with Control
+  const zoomin = true;
+  if (e.ctrlKey) {
+    zoomin = false;
+  }
+
+  // Pan with Shift
+  const zoomfactor = 2;
+  if (e.shiftKey) {
+    zoomfactor = 1;
+  }
+
+  console.log("pos.x, pos.y ", pos.x, pos.y);
+
+  zoomFractal(pos.x, pos.y, zoomfactor, zoomin);
+
+  //   universe.generate(
+  //   iterations,
+  //   zoom,
+  //   offsetx,
+  //   offsety,
+  //   panx,
+  //   pany,
+  // )
+
   universe.tick();
   drawCells();
-  requestAnimationFrame(renderLoop);
 };
 
-universe.tick();
-drawCells();
+const getMousePos = (canvas, e) => {
+  const rect = canvas.getBoundingClientRect();
+  return {
+    x: Math.round(
+      ((e.clientX - rect.left) / (rect.right - rect.left)) * canvas.width
+    ),
+    y: Math.round(
+      ((e.clientY - rect.top) / (rect.bottom - rect.top)) * canvas.height
+    ),
+  };
+};
 
-// requestAnimationFrame(renderLoop);
+const init = () => {
+  // Add mouse events
+  canvas.addEventListener("mousedown", onMouseDown);
+
+  universe.tick();
+  drawCells();
+
+  const cellsPtr = universe.cells();
+  const cells = new Uint16Array(memory.buffer, cellsPtr, width * height);
+
+  // const renderLoop = () => {
+  //   universe.tick();
+  //   drawCells();
+  //   requestAnimationFrame(renderLoop);
+  // };
+  // requestAnimationFrame(renderLoop);
+};
+
+init();
